f13615d96f0a401c830b0f0db50cc0e7d801e9ae
Kingsley Udenewu <kingzpacking@gmail.com>
2019-02-04 11:08:02 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  Container,
+  Col,
+  Row,
+  ListGroup,
+  ListGroupItem,
+  Card,
+  CardText,
+  CardBody,
+  CardTitle
+} from "reactstrap";
+import NavbarMenu from "./components/NavbarMenu";
+import ModalForm from "./components/ModalForm";
 
-class App extends Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      show: false,
+      alert: null
+    };
+
+    this.handleClose = this.handleClose.bind(this);
+    this.handleShow = this.handleShow.bind(this);
+  }
+
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {this.state.alert}
+        <NavbarMenu onClick={this.handleShow} />
+        <Container fluid>
+          <Row>
+            <ModalForm show={this.state.show} onClose={this.handleClose} />
+            <Col xs="3">
+              <ListGroup>
+                <ListGroupItem>Home</ListGroupItem>
+                <ListGroupItem>Profile</ListGroupItem>
+                <ListGroupItem>About</ListGroupItem>
+                <ListGroupItem
+                  onClick={this.handleShow}
+                  style={{ cursor: "pointer" }}
+                >
+                  Feedback
+                </ListGroupItem>
+              </ListGroup>
+            </Col>
+            <Col xs="9">
+              <Card>
+                <CardBody>
+                  <CardTitle>Hello world</CardTitle>
+                  <CardText>This is the card text</CardText>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
 }
-
-export default App;
---
+++ src/components/ModalForm.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import {
+  FormGroup,
+  Form,
+  Input,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button
+} from "reactstrap";
+import swal from "sweetalert";
+/**
+ * <SweetAlert success title="Good job!" onConfirm={this.props.onConfirm}>
+        You clicked the button!
+      </SweetAlert>
+ */
+
+export default class ModalForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      feedback: "",
+      showSweetAlert: false
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.feedback);
+
+    this.setState({ feedback: "" });
+    swal("Notification", "Feedback Submited", "success");
+  }
+
+  render() {
+    return (
+      <div>
+        <Modal isOpen={this.props.show} toggle={this.props.onClose}>
+          <ModalHeader toggle={this.props.onClose}>ASend Feedback</ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.onSubmit}>
+              <FormGroup>
+                <label htmlFor="">Enter Your Feedback</label>
+                <Input
+                  name="feedback"
+                  type="textarea"
+                  value={this.state.feedback}
+                  onChange={this.onChange}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Button color="success" type="submit">
+                  Save
+                </Button>
+              </FormGroup>
+            </Form>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={this.props.onClose}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
+}
